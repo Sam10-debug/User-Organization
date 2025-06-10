595a46f729a7ff21d2e2e44b14c9d1201304e1ae
Emorinken <emorinkensam@gmail.com>
2025-06-10 22:04:58 +0100

    added lombok

--- src/main/java/com/example/week3_task/model/Organisation.java
+++ src/main/java/com/example/week3_task/model/Organisation.java
@@ -1,60 +1,36 @@
 package com.example.week3_task.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.ManyToMany;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.Set;
+import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Organisation {
     @Id
     @GeneratedValue
-    private Long orgId;
+
+    private UUID orgId;
+    @Column(nullable = false)
     private String name;
+    @Column(nullable = false)
     private String description;
 
-    public Organisation() {
-    }
 
-    public Set<User> getUser() {
-        return user;
-    }
 
-    public void setUser(Set<User> user) {
-        this.user = user;
-    }
 
-    @ManyToMany(mappedBy = "organisations")
-    private Set<User> user;
+    @ManyToMany
+    private Set<User> users;
 
-    public Organisation(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
 
-    public Long getOrgId() {
-        return orgId;
-    }
 
-    public void setOrgId(Long orgId) {
-        this.orgId = orgId;
-    }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
--- src/main/java/com/example/week3_task/model/User.java
+++ src/main/java/com/example/week3_task/model/User.java
@@ -1,94 +1,49 @@
 package com.example.week3_task.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
 
 import java.util.Set;
+import java.util.UUID;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
+@Table(name = "userTable", uniqueConstraints = @UniqueConstraint(columnNames={"email"}))
 public class User {
     @Id
     @GeneratedValue
-    private Long userId;
+    private UUID userId;
+    @Column(nullable = false)
     private  String firstName;
+    @Column(nullable = false)
     private  String lastName;
+    @Column(nullable = false)
     private  String email;
+    @Column(nullable = false)
     private  String phone;
+    @Column(nullable = false)
     private  String password;
 
+    @ManyToMany(mappedBy = "users")
 
-    public User() {
+private Set<Organisation> organisations;
+
+    @PostPersist
+    public void createDefaultOrganisation() {
+        Organisation defaultOrg = new Organisation();
+        defaultOrg.setName(this.firstName + "'s Organisation");
+        this.organisations.add(defaultOrg);  // User → Org
+        defaultOrg.getUsers().add(this);    // Org → User (bidirectional sync)
     }
 
-    public User(String firstName, String lastName, String email, String phone, String password) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-    }
 
 
-    public Set<Organisation> getOrganisation() {
-        return organisation;
-    }
-
-    public void setOrganisation(Set<Organisation> organisation) {
-        this.organisation = organisation;
-    }
-
-    @ManyToMany
-@JoinTable(
-        name = "user_organisation",
-        joinColumns = @JoinColumn(name = "user_id"),
-        inverseJoinColumns = @JoinColumn(name = "organisation_id")
-)
-private Set<Organisation> organisation;
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getUserId() {
-        return userId;
-    }
-
-    public void setUserId(Long id) {
-        this.userId = id;
-    }
 }
